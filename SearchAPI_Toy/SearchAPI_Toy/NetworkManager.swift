56c9a111f04746ceceee210e0235418bbbfdb8fc
B0X <83914919+BOLTB0X@users.noreply.github.com>
2023-06-02 17:56:02 +0900

    초기 셋팅

---
+++ SearchAPI_Toy/SearchAPI_Toy/NetworkManager.swift
@@ -0,0 +1,19 @@
+//
+//  NetworkManager.swift
+//  SearchAPI_Toy
+//
+//  Created by KyungHeon Lee on 2023/06/02.
+//
+
+import Foundation
+
+enum NetworkManager {
+    static var apiKey: String? {
+        guard let apiKey = Bundle.main.object(forInfoDictionaryKey: "API_KEY") as? String else {
+            fatalError("Info.plist안에 API_KEY가 연결이 안됨")
+        }
+        return apiKey
+    }
+    // 헤드라인
+    static let webURL:String = "https://dapi.kakao.com/v2/search/web"
+}
